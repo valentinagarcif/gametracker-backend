db67e66d035bd399c3e2fd31d64a64842c413dee
valentinagarcif <valentinagarcif@gmail.com>
2025-11-12 19:06:18 -0500

    servidor express funcionando con rutas api

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req,res) => {
+    res.json({
+        message: '¡Mi servidor de GameTracker está funcionando!', 
+        status: 'success', 
+        student:'Valentina García'
+    });
+});
+
+app.get('/', (req,res) => {
+    res.json({
+        message: 'Bienvenido a GameTracker API',
+        endpoints: {
+            test: '/api/test',
+            games: '/api/games',
+            reviews: '/api/reviews'
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+    console.log(`GameTracker Backend activo!`);
+});
